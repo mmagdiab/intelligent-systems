d7b1c42a2087ae320e2c1dbcf3b741375b181b33
Mohamed Magdy <14momagdi@gmail.com>
2023-03-12 14:37:49 +0200

    section changes

--- sheet01/problem03.py
+++ sheet01/problem03.py
@@ -11,13 +11,22 @@ def is_arranged(puzzle):
             next_value = next_value + 1 if puzzle[r][c] != 0 else next_value
     return True
 
+def is_arranged_with_goal(start, goal):
+    rows, columns = len(start), len(start[0])
+    for r in range(rows):
+        for c in range(columns):
+            if start[r][c] != goal[r][c]:
+                return False
+    return True
 
 puzzle_1 = [[1, 0, 2],
             [3, 4, 5],
             [6, 7, 8]]
 
-puzzle_2 = [[2, 0, 4],
-            [3, 1, 5],
-            [6, 7, 8]]
-print(is_arranged(puzzle_1))
-print(is_arranged(puzzle_2))
+puzzle_2 = [[1, 0, 4],
+            [5, 3, 6],
+            [7, 2, 8]]
+# print(is_arranged(puzzle_1))
+# print(is_arranged(puzzle_2))
+
+print(is_arranged_with_goal(puzzle_2, puzzle_2))
